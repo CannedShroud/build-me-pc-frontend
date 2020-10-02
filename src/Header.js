40c1d0271c75d5108e0eff939fdac183c25137ed
Vyom Desai <hypersync112@gmail.com>
2020-10-02 22:09:08 +0530

    Build header

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,5 @@
+import { IconButton } from "@material-ui/core";
+import { Brightness4Outlined, EmojiFlags } from "@material-ui/icons";
 import React from "react";
 import "./Header.css";
 
@@ -5,11 +7,19 @@ function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <div className="header__logo">{/* Logo */}</div>
+        <div className="header__logo">
+          <h1>
+            BUILD<span> ME </span> PC
+          </h1>
+        </div>
       </div>
       <div className="header__right">
-        {/* Region */}
-        {/* Light/Dark theme */}
+        <IconButton>
+          <EmojiFlags />
+        </IconButton>
+        <IconButton>
+          <Brightness4Outlined />
+        </IconButton>
       </div>
     </div>
   );
