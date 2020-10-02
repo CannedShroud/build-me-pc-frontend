f27a147cc97785374124e9c2062501d13bad48be
Vyom Desai <hypersync112@gmail.com>
2020-10-02 21:48:52 +0530

    Start work on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>I am App</h1>
-      
+      <div className="app__header">
+        <Header />
+      </div>
+      <div className="app__body">
+        <h1>I am App</h1>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <div className="header__logo">{/* Logo */}</div>
+      </div>
+      <div className="header__right">
+        {/* Region */}
+        {/* Light/Dark theme */}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
