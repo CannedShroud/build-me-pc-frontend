4878dd98ad9cc560d62c5a57c85fd0d494bcd3e9
Vyom Desai <hypersync112@gmail.com>
2020-10-02 23:40:22 +0530

    Finish Card structure

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Card.css";
 
 function Card() {
-  return <div className="card">I am Card</div>;
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img
+          src="https://store-images.s-microsoft.com/image/apps.46452.13835473807186336.9510cd97-f71a-49b0-9e5c-0877f4bc9de3.611f3905-926e-456a-b549-6b68564b2769?mode=scale&q=90&h=300&w=200&background=%23FFFFFF"
+          alt=""
+        />
+      </div>
+      <div className="card__title">
+        <h3>WarZone</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
