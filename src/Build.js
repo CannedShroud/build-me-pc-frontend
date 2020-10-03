62583099b841bbc6c9847ed6ee0694cc3cdeed6a
Vyom Desai <hypersync112@gmail.com>
2020-10-03 09:36:56 +0530

    Build basic BuildPage

--- src/Build.js
+++ src/Build.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Build.css";
+import Header from "./Header";
 
 function Build() {
-  return <div className="build">I Am build page</div>;
+  return (
+    <div className="build">
+      <div className="build__header">
+        <Header />
+      </div>
+      <div className="build__body">
+        {/* BuildCard */}
+        {/* BuildCard */}
+      </div>
+    </div>
+  );
 }
 
 export default Build;
