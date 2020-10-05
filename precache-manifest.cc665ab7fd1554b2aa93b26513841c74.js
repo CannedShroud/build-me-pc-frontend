f8e134727a0deb5a8acb5ee3d7952e7a02f5351f
Vyom Desai <hypersync112@gmail.com>
2020-10-05 12:09:21 +0530

    Updates

---
+++ precache-manifest.cc665ab7fd1554b2aa93b26513841c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4108fa641054b86d54629e78c53fb32",
+    "url": "/dental-studio-gds/index.html"
+  },
+  {
+    "revision": "477a54c9f35a17666e67",
+    "url": "/dental-studio-gds/static/css/main.c3590bb5.chunk.css"
+  },
+  {
+    "revision": "b2f59a3063c2fe5a35c5",
+    "url": "/dental-studio-gds/static/js/2.0aeb6cc0.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/dental-studio-gds/static/js/2.0aeb6cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477a54c9f35a17666e67",
+    "url": "/dental-studio-gds/static/js/main.2b4db2f9.chunk.js"
+  },
+  {
+    "revision": "1b0ea696324a8f1bdce4",
+    "url": "/dental-studio-gds/static/js/runtime-main.cac05824.js"
+  },
+  {
+    "revision": "01149f4a833499b5c9ad1ca29811f3cb",
+    "url": "/dental-studio-gds/static/media/demopc.01149f4a.png"
+  }
+]);
