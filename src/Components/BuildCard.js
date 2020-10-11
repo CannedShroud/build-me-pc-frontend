3cb2a80f0e2183b61bbd9375013bee08c5f7ec90
Vyom Desai <hypersync112@gmail.com>
2020-10-11 13:05:28 +0530

    Repoonsive Build Page now!

--- src/Components/BuildCard.js
+++ src/Components/BuildCard.js
@@ -14,22 +14,24 @@ function BuildCard({ docId }) {
   }
   return (
     <div className="buildcard">
-      <div className="buildcard__title">
-        <h1>{build.details?.name}</h1>
-      </div>
-      <div className="buildcard__image">
-        <img src={build.case?.img} alt="" className="buildcard__case" />
-        <div className="buildcard__partsImg">
-          <img src={build.cpu?.img} alt="" />
-          <img src={build.gpu?.img} alt="" />
+      <div className="buildcard__body">
+        <div className="buildcard__title">
+          <h1>{build.details?.name}</h1>
         </div>
-      </div>
-      <div className="buildcard__price">
-        <h3>~$1000</h3>
-      </div>
-      <div className="buildcard__benchmarks">
-        <h4>{`Cinebench: ${build.benchmarks?.cinebench}`}</h4>
-        <h4>{`3D Mark: ${build.benchmarks?.threeDMarkTSpy}`}</h4>
+        <div className="buildcard__image">
+          <img src={build.case?.img} alt="" className="buildcard__case" />
+          <div className="buildcard__partsImg">
+            <img src={build.cpu?.img} alt="" />
+            <img src={build.gpu?.img} alt="" />
+          </div>
+        </div>
+        <div className="buildcard__price">
+          <h3>~$1000</h3>
+        </div>
+        <div className="buildcard__benchmarks">
+          <h4>{`Cinebench: ${build.benchmarks?.cinebench}`}</h4>
+          <h4>{`3D Mark: ${build.benchmarks?.threeDMarkTSpy}`}</h4>
+        </div>
       </div>
     </div>
   );
