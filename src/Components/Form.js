a4456724a5462af214e820a3e7bf12af42cb839f
Vyom Desai <hypersync112@gmail.com>
2020-10-13 13:50:36 +0530

    [WIP]Continue with the form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,8 +1,113 @@
-import React from "react";
+import {
+  Checkbox,
+  Container,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Button,
+} from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import React, { useState } from "react";
 import "../Styles/Form.css";
+import Brain from "../Logic/Brain";
 
-function Form() {
-  return <div className="form"> I am Form</div>;
+function Form(props) {
+  const brain = Brain("games", "games");
+  const games = brain.all;
+  const [step, setStep] = useState(1);
+  const [inputFields, setInputFields] = useState([
+    {
+      budget: 0,
+      games: [],
+      extras: {
+        streaming: true,
+        videoEditing: true,
+      },
+    },
+  ]);
+  return (
+    <Container>
+      <div className="form">
+        <div className="form__budget form__step">
+          <div className="form__info">
+            <h2>Step ONE!</h2>
+            <h3>
+              PC Building is a place where we can get carried away because of
+              the amazing RGB accents and lights products come with. <br />
+              So let's mark our boundaries.
+            </h3>
+            <h4>Enter your budget</h4>
+          </div>
+          <TextField
+            required="true"
+            label="Budget"
+            type="number"
+            defaultValue="1000"
+            onChange={(event) => console.log(event.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+          />
+          <Button className="form__btn" onClick={() => setStep(2)}>
+            NEXT
+          </Button>
+        </div>
+        <div
+          className="form__games form__step"
+          style={step >= 2 ? null : { display: "none" }}
+        >
+          <div className="form__info">
+            <h2>Step TWO!</h2>
+            <h3>
+              A Gaming PC is not even remotely a "Gaming PC" if you don't GAME
+              on it. <br />
+              So let's fill out cart
+            </h3>
+            <h4>Pick your favourite Game</h4>
+          </div>
+          <Autocomplete
+            id="combo-box-demo"
+            options={games}
+            getOptionLabel={(option) => option.title}
+            style={{ width: 300 }}
+            required="true"
+            renderInput={(params) => (
+              <TextField {...params} label="Game" variant="outlined" />
+            )}
+          />
+          <Button className="form__btn" onClick={() => setStep(3)}>
+            NEXT
+          </Button>
+        </div>
+        <div
+          className="form__extras form__step"
+          style={step >= 3 ? null : { display: "none" }}
+        >
+          <div className="form__info">
+            <h2>Step THREE!</h2>
+            <h3>
+              Billioniare by Day, Superhero by night? BatMan? <br />
+              What is your SuperPower?
+            </h3>
+            <h4>Pick your candy</h4>
+          </div>
+          <FormControlLabel control={<Checkbox name="code" />} label="I Code" />
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox name="streamer" />}
+              label="I Stream"
+            />
+            <FormControlLabel
+              control={<Checkbox name="edit" />}
+              label="I Edit Videos"
+            />
+          </FormGroup>
+          <Button className="form__btn">NEXT</Button>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default Form;
--- src/Page/Estimator.js
+++ src/Page/Estimator.js
@@ -9,55 +9,62 @@ function Estimator() {
     <div className="estimator">
       <Header />
       <div className="estimator__body">
-        <div className="estimator__content">
-          <div className="estimator__intro">
-            <h4>
-              WELCOME TO THE <span>ESTIMATOR</span>
-            </h4>
-            <h1>
-              <span>PC Building</span> Made Simple
-            </h1>
-            <p>
-              Here is where part picking gets simple, you just input the
-              critera, <br />
-              and get all the parts you need shortlisted. <br />
-              <span>Here is how it will go</span>
-            </p>
+        <div className="estimator__top">
+          <div className="estimator__content">
+            <div className="estimator__intro">
+              <h4>
+                WELCOME TO THE <span>ESTIMATOR</span>
+              </h4>
+              <h1>
+                <span>PC Building</span> Made Simple
+              </h1>
+              <p>
+                Here is where part picking gets simple, you just input the
+                critera, <br />
+                and get all the parts you need shortlisted. <br />
+                <span>Here is how it will go</span>
+              </p>
+            </div>
+            <div className="estimator__steps">
+              <ul>
+                <li>
+                  <div className="estimator__step">
+                    <div className="estimator__index">1</div>
+                    <h3>Pick your Price</h3>
+                  </div>
+                </li>
+                <li>
+                  <div className="estimator__step">
+                    <div className="estimator__index">2</div>
+                    <h3>Choose your Games</h3>
+                  </div>
+                </li>
+                <li>
+                  <div className="estimator__step">
+                    <div className="estimator__index">3</div>
+                    <h3>Umm... Extras?</h3>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <Button
+              href="#estimator__form"
+              className="estimator__getStartedBtn"
+            >
+              GET STARTED
+            </Button>
           </div>
-          <div className="estimator__steps">
-            <ul>
-              <li>
-                <div className="estimator__step">
-                  <div className="estimator__index">1</div>
-                  <h3>Pick your price</h3>
-                </div>
-              </li>
-              <li>
-                <div className="estimator__step">
-                  <div className="estimator__index">2</div>
-                  <h3>Choose your budget</h3>
-                </div>
-              </li>
-              <li>
-                <div className="estimator__step">
-                  <div className="estimator__index">3</div>
-                  <h3>Umm... Extras?</h3>
-                </div>
-              </li>
-            </ul>
+          <div className="estimator__caseImg">
+            <img
+              src="https://cdn.shopify.com/s/files/1/2507/5448/products/mb520-122-1200x1200_683_compact.png?v=1597971261"
+              alt=""
+            />
           </div>
-          <Button href={""} className="estimator__getStartedBtn">
-            GET STARTED
-          </Button>
         </div>
-        <div className="estimator__caseImg">
-          <img
-            src="https://cdn.shopify.com/s/files/1/2507/5448/products/mb520-122-1200x1200_683_compact.png?v=1597971261"
-            alt=""
-          />
-        </div>
-        <div className="estimator__form">
-          <Form />
+        <div className="estimator__bottom">
+          <div className="estimator__form">
+            <Form />
+          </div>
         </div>
       </div>
     </div>
